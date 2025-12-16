a511f2f4da2e1bc93590fb8570d556d57f796907
bonzoholda <bhumibeka@gmail.com>
2025-12-17 05:18:56 +0700

    Update SphygmosController.ts

--- src/abi/SphygmosController.ts
+++ src/abi/SphygmosController.ts
@@ -33,5 +33,27 @@ export const SPHYGMOS_CONTROLLER_ABI = [
     "stateMutability": "view",
     "inputs": [{ "name": "", "type": "address" }],
     "outputs": [{ "type": "uint256" }]
-  }
+  },
+{
+  "type": "function",
+  "name": "depositPush",
+  "stateMutability": "nonpayable",
+  "inputs": [{ "name": "usdtAmount", "type": "uint256" }],
+  "outputs": []
+},
+{
+  "type": "function",
+  "name": "stake",
+  "stateMutability": "nonpayable",
+  "inputs": [{ "name": "amount", "type": "uint256" }],
+  "outputs": []
+},
+{
+  "type": "function",
+  "name": "claimMinerRewards",
+  "stateMutability": "nonpayable",
+  "inputs": [],
+  "outputs": []
+}
+  
 ] as const;
