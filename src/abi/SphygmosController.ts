e0023a4aec556cba221d892ce1a95d5868d24c89
bonzoholda <bhumibeka@gmail.com>
2025-12-17 06:25:00 +0700

    Update SphygmosController.ts

--- src/abi/SphygmosController.ts
+++ src/abi/SphygmosController.ts
@@ -34,26 +34,62 @@ export const SPHYGMOS_CONTROLLER_ABI = [
     "inputs": [{ "name": "", "type": "address" }],
     "outputs": [{ "type": "uint256" }]
   },
-{
-  "type": "function",
-  "name": "depositPush",
-  "stateMutability": "nonpayable",
-  "inputs": [{ "name": "usdtAmount", "type": "uint256" }],
-  "outputs": []
-},
-{
-  "type": "function",
-  "name": "stake",
-  "stateMutability": "nonpayable",
-  "inputs": [{ "name": "amount", "type": "uint256" }],
-  "outputs": []
-},
-{
-  "type": "function",
-  "name": "claimMinerRewards",
-  "stateMutability": "nonpayable",
-  "inputs": [],
-  "outputs": []
-}
+  {
+    "type": "function",
+    "name": "depositPush",
+    "stateMutability": "nonpayable",
+    "inputs": [{ "name": "usdtAmount", "type": "uint256" }],
+    "outputs": []
+  },
+  {
+    "type": "function",
+    "name": "stake",
+    "stateMutability": "nonpayable",
+    "inputs": [{ "name": "amount", "type": "uint256" }],
+    "outputs": []
+  },
+  {
+    "type": "function",
+    "name": "claimMinerRewards",
+    "stateMutability": "nonpayable",
+    "inputs": [],
+    "outputs": []
+  }
+  {
+    "type": "function",
+    "name": "seederPoolBalance",
+    "stateMutability": "view",
+    "inputs": [],
+    "outputs": [{ "type": "uint256" }]
+  },
+  {
+    "type": "function",
+    "name": "pairedPoolBalance",
+    "stateMutability": "view",
+    "inputs": [],
+    "outputs": [{ "type": "uint256" }]
+  },
+  {
+    "type": "function",
+    "name": "triggerPoolBalance",
+    "stateMutability": "view",
+    "inputs": [],
+    "outputs": [{ "type": "uint256" }]
+  },
+  {
+    "type": "function",
+    "name": "totalStakedSMOS",
+    "stateMutability": "view",
+    "inputs": [],
+    "outputs": [{ "type": "uint256" }]
+  },
+  {
+    "type": "function",
+    "name": "dripRatePerSecond",
+    "stateMutability": "view",
+    "inputs": [],
+    "outputs": [{ "type": "uint256" }]
+  }
+
   
 ] as const;
