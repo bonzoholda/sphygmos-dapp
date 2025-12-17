c7b974ad28776a8bcfbefe7e9bd3ae604dfac82f
bonzoholda <bhumibeka@gmail.com>
2025-12-17 12:22:24 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Actions } from "./components/Actions";
 import Stats from "./components/Stats";
 import { fmt } from "./utils/format";
 import DripStats from "./components/DripStats";
-import Logo from "./assets/logo.png";
+import Logo from "./assets/logo.svg";
 
 
 
