49e9311520c30a3f711cc6e94146a82f016e2945
bonzoholda <bhumibeka@gmail.com>
2025-12-17 21:55:47 +0700

    Create pancake.ts

---
+++ src/constants/pancake.ts
@@ -0,0 +1,27 @@
+export const PANCAKE_ROUTER_ADDRESS = "0x9Ac64Cc6e4415144C455bd8E4837fea55603e5c3"; // BSC Testnet Router
+
+export const PANCAKE_ROUTER_ABI = [
+  {
+    "inputs": [
+      {"internalType": "uint256", "name": "amountIn", "type": "uint256"},
+      {"internalType": "address[]", "name": "path", "type": "address[]"}
+    ],
+    "name": "getAmountsOut",
+    "outputs": [{"internalType": "uint256[]", "name": "amounts", "type": "uint256[]"}],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {"internalType": "uint256", "name": "amountIn", "type": "uint256"},
+      {"internalType": "uint256", "name": "amountOutMin", "type": "uint256"},
+      {"internalType": "address[]", "name": "path", "type": "address[]"},
+      {"internalType": "address", "name": "to", "type": "address"},
+      {"internalType": "uint256", "name": "deadline", "type": "uint256"}
+    ],
+    "name": "swapExactTokensForTokens",
+    "outputs": [{"internalType": "uint256[]", "name": "amounts", "type": "uint256[]"}],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+] as const;
