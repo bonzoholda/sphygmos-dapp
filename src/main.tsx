6efe0c93fed63837ce6abe3efb6b49ee79ab9252
bonzoholda <bhumibeka@gmail.com>
2025-12-17 11:17:23 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import "./index.css";
 import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { wagmiConfig } from "./config/wagmi";
-import { RefreshProvider } from "./context/RefreshContext";
 
 const queryClient = new QueryClient();
 
@@ -14,9 +13,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <RefreshProvider>
-          <App />
-        </RefreshProvider>
+
+        <App />
+
       </QueryClientProvider>
     </WagmiProvider>
   </React.StrictMode>
